4f032ddf296616e4b4f07cfe3b27adb82bd05115
panachai1 <gromkromz@gmail.com>
2025-10-08 09:50:06 +0700

    commit testing one

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,19 +1,17 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test('fill and submit the basic form', async ({ page }) => {
+  await page.goto('http://localhost:9000');
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  await page.fill('input[label="Full Name"]', 'สมชาย ทดสอบ');
+  await page.fill('input[label="Email"]', 'somchai@test.com');
+  await page.fill('input[label="Password"]', '123456');
+
+  await page.click('button:has-text("Submit")');
+
+  // ตรวจสอบว่ามี alert หรือข้อความขึ้น
+  page.once('dialog', async (dialog) => {
+    console.log('Alert message:', dialog.message());
+    await dialog.dismiss();
+  });
 });
-
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-});
-
